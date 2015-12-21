9599a889e61655c48d9ac05de46ed4b249324b02
jay park <jaypark@gmail.com>
2015-12-21 15:00:42 +0900

    시도 이름 추가

--- crawler/candi_list.rb
+++ crawler/candi_list.rb
@@ -14,6 +14,7 @@ NEC_SERVER= 'info.nec.go.kr'
 PIC_PREFIX= "http://#{NEC_SERVER}"
 
 
+#
 # 이 URL로 전체 후보를 다 가져올수 있다. 
 # 브라우저에서 보면, 시도코드를 선택하게 되어있지만, 
 # 시도코드를 0으로 전송하면 전체 시도의 후보를 가져온다.
@@ -24,8 +25,9 @@ CANDI_LIST= '/electioninfo/electionInfo_report.xhtml'+
             '&requestURI=%2Felectioninfo%2F0020160413%2Fpc%2Fpcri03_ex.jsp'+
             '&topMenuId=PC&secondMenuId=PCRI03&menuId='+
             '&statementId=PCRI03_%232'+
-            '&electionCode=2&cityCode=0&sggCityCode=0'+
+            '&electionCode=2'+
             '&townCode=-1&sggTownCode=0&x=19&y=14'
+            '&electionCode=2&cityCode=0&sggCityCode=0'+
 
 # 후보 통계 페이지
 CANDI_SUM = '/electioninfo/electionInfo_report.xhtml'+
@@ -36,9 +38,22 @@ CANDI_SUM = '/electioninfo/electionInfo_report.xhtml'+
 
 
 
+
 list_html   = Net::HTTP.get(NEC_SERVER, CANDI_LIST)
 list_doc    = Nokogiri::HTML(list_html)
+
+# 2016 선거구 목록 가져오기
+city_list   = {}
+city_html   = list_doc.css("select#cityCode")
+city_html.css("option").each do |city|
+  puts city.attribute_nodes[0].to_s
+  puts city.content
+  city_list[city.attribute_nodes[0].to_s] = city.content
+end
+puts city_list
+
 candi_list  = []
+
 
 list_doc.css("table#table01").css("tr").each_with_index do |tr, idx|
 
@@ -51,6 +66,13 @@ list_doc.css("table#table01").css("tr").each_with_index do |tr, idx|
     # photo = http://info.nec.go.kr/photo_20160413/Sd1100/Gsg1101/Sgg2110101/Hb100118435/gicho/100118435.jpg
 
     h[:id]              = photo.scan( /\/gicho\/(.+)\./).first[0].to_s
+
+    
+    # Sd 가 시도를 의미하고, 뒤의 숫자와 select#cityCode 의 숫자를 이용하면 시도이름을 얻을 수 있다.
+
+    h[:city]            = city_list[photo.scan( /\/Sd(.+)\/Gsg/).first[0].to_s]
+
+
     h[:district]        = candi_array.at(0).content
     h[:party]           = candi_array.at(1).content
     h[:picture]         = PIC_PREFIX+candi_array.at(2).children[1].attribute_nodes[1]
